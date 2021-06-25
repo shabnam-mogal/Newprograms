c4cd689e4649df66b20b30debfd3e7f4c4d22849
salman <salman@salman-PC>
2021-06-25 17:34:57 +0530

    code is ready

---
+++ Newprograms/src/abcd/Compareprogram.java
@@ -0,0 +1,21 @@
+package abcd;
+
+public class Compareprogram {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		//sub string programs
+String a="shabnam";
+for( int i=0;i<a.length();i++){
+	for(int j=i+1;j<=a.length();j++){
+		System.out.println(a.substring(i, j));
+	}
+}
+
+
+	}
+
+}
---
+++ Newprograms/src/abcd/Substringpro.java
@@ -0,0 +1,69 @@
+package abcd;
+
+import java.util.Scanner;
+
+public class Substringpro {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		
+	//sub string programs
+/*
+		String reverse="";
+		Scanner a=new Scanner(System.in);
+		System.out.println("enter the string is reverse");
+		String b=a.nextLine();
+		for(int i=b.length()-1;i>=0;i--){
+			reverse=reverse+b.charAt(i);
+			if(b.equals(reverse)){
+				System.out.println("it is a reverse");
+			}
+			else {
+				System.out.println("it is not a reverse");
+			}
+
+	     String original, reverse = "";
+
+	     Scanner in = new Scanner(System.in); 
+
+	     System.out.println("Enter a string to reverse");
+
+	     original = in.nextLine();
+
+	     int length = original.length();
+
+	     for (int i = length - 1 ; i >= 0 ; i--)
+
+	       reverse = reverse + original.charAt(i); 
+
+	     System.out.println("Reverse of the string: " + reverse);
+		
+		String reverse1="";
+		Scanner a=new Scanner(System.in);
+		System.out.println("enter the reverse value");
+		
+		String b=a.nextLine();
+		for(int i=b.length()-1;i>=0;i--){
+			reverse1=reverse1+b.charAt(i);
+			System.out.println("reverse of string:" +reverse1);*/
+		
+		
+		String reverse1="";
+		StringBuffer a=new StringBuffer(reverse1);
+		
+		
+		
+		}
+		
+		}
+		
+		
+		
+		
+
+	
+
+
