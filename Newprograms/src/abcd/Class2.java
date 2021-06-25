3627454adb3732bb3013dcb47bcc4ea30c47cef9
salman <salman@salman-PC>
2021-06-25 17:55:44 +0530

    file is pull

---
+++ Newprograms/src/abcd/Class2.java
@@ -0,0 +1,36 @@
+
+		
+		
+		
+		package abcd;
+
+		import java.util.Scanner;
+
+		public class Class2 {
+
+			public static void main(String[] args) {
+
+				
+				
+				
+				
+		String reverse="";
+		Scanner a=new Scanner(System.in);
+		System.out.println("enter the palendrome value");
+			String b=a.nextLine();
+			for(int i=b.length()-1;i>0;i--){
+		reverse=reverse+b.charAt(i);
+		if(b.equals(reverse)){
+			System.out.println("it is palindome");
+		}
+		else {
+			System.out.println("it is not a palindrome");
+		}
+			}
+
+		}
+		
+
+	}
+
+
