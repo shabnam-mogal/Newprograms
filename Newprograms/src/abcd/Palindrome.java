ce9a817916a745b80e1b4ce0e1a059086ae2b94a
salman <salman@salman-PC>
2021-06-25 18:07:58 +0530

    ffff

--- Newprograms/src/abcd/Palindrome.java
+++ Newprograms/src/abcd/Palindrome.java
@@ -10,7 +10,7 @@ public class Palindrome {
 		
 		
 		
-String reverse="";
+String reverse="shabnamshabbu";
 Scanner a=new Scanner(System.in);
 System.out.println("enter the palendrome value");
 	String b=a.nextLine();
